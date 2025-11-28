e7327c1a60e623e70d7cb0f814aa2d2726c83d14
leonfagundes27 <leonfagundes7@gmail.com>
2025-11-27 21:52:47 -0300

    fix/ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,15 @@ module.exports = {
   apps: [
     {
       name: "fastapi",
-      script: "uvicorn",
-      args: ["app.main:app", "--host", "0.0.0.0", "--port", "8001"],
-      interpreter: "none"
+      script: "./venv/bin/uvicorn",
+      args: "app.main:app --host 0.0.0.0 --port 8001 --reload",
+      cwd: "/root/voice-auth-microservice",
+      interpreter: "none",
+      env: {
+        PYTHONPATH: "/root/voice-auth-microservice"
+      },
+      max_restarts: 10,
+      min_uptime: "10s"
     }
   ]
 }
